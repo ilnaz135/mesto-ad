9404ac451089cb789da039288cb2bb0dd06bb736
ilnaz135 <ilnazmuftiev125@gmail.com>
2026-01-12 21:05:56 +0500

    Add files via upload

---
+++ src/scripts/components/api.js
@@ -0,0 +1,78 @@
+// Токен
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "75ad4f03-c299-4ec1-9dff-67bbf5b23629",
+    "Content-Type": "application/json",
+  },
+};
+
+// Получаем ответ с сервера
+const getResponseData = (res) => {
+  return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}; 
+
+// Получаем информацию о пользователях с сервера
+export const getUserInfo = () => {
+  return fetch(`${config.baseUrl}/users/me`, { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
+
+// Получаем карточки пользователей с сервера
+export const getCardList = () => {
+  return fetch(`${config.baseUrl}/cards`, { // Запрос к API-серверу
+    headers: config.headers, // Подставляем заголовки
+  }).then(getResponseData);  // Проверяем успешность выполнения запроса
+};
+
+// Записываем информацию о пользователе на странице с сервера
+export const setUserInfo = ({ name, about }) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      about,
+    }),
+  }).then(getResponseData);
+};
+
+// Добавляем аватар пользователя на странице с сервера
+export const setUserAvatar = ({ avatar }) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar
+    }),
+  }).then(getResponseData);
+};
+
+// Добавляем карточку пользователя на сервер
+export const addUserCard = ({ name, link }) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify({
+      name,
+      link
+    }),
+  }).then(getResponseData);
+};
+
+// Удаляем карточку пользователя на сервере
+export const deleteUserCard = (cardId) => {
+  return fetch(`${config.baseUrl}/cards/${cardId}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(getResponseData);
+};
+
+// Меняем статус кнопки лайка на сервере
+export const changeLikeCardStatus = (cardID, isLiked) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardID}`, {
+    method: isLiked ?  "DELETE" : "PUT",
+    headers: config.headers,
+  }).then((res) => getResponseData(res));
+};
